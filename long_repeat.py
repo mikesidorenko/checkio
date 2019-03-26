53d42bff96e17f50b459d4cd5a67a5c2b6ae03fd
Mike <mysmobile@gmail.com>
2019-03-26 18:55:18 +0200

    long repeat

--- long_repeat.py
+++ long_repeat.py
@@ -1,7 +1,11 @@
 def long_repeat(line):
-	import re
-	a = re.split(r'(.+)', line)
-	return max(len(b) for b in a)
-
-
-print(long_repeat('abababaab'))
+	if line == '':
+		return 0
+	else:
+		import re
+		fuck = re.compile(r'(.)\1+')
+		refuck = [match.group() for match in fuck.finditer(line)]
+		if refuck == []:
+			return 1
+		else:
+			return max(len(x) for x in refuck)
