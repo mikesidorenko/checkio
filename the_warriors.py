d503408bac0bc4a537ee4072ec933f1bd78326b2
Mike <mysmobile@gmail.com>
2019-03-27 09:21:11 +0200

    used is_alive as property to be autodependant from health

--- the_warriors.py
+++ the_warriors.py
@@ -2,7 +2,10 @@ class Warrior:
 	def __init__(self):
 		self.health = 50
 		self.attack = 5
-		self.is_alive = True
+	
+	@property
+	def is_alive(self):			# automatically changes when health
+	   return self.health > 0   # drops to zero or below
 
 class Knight(Warrior):
 	def __init__(self):
@@ -11,10 +14,8 @@ class Knight(Warrior):
 
 def fight(unit_1, unit_2):
 	while unit_1.is_alive and unit_2.is_alive:             # resume fight condition
-		unit_2.health = unit_2.health - unit_1.attack      # unit_1 attacks
-		unit_2.is_alive = unit_2.health > 0                #kill when <= 0
+		unit_2.health -= unit_1.attack                     # unit_1 attacks
 		if unit_2.is_alive:                                #check if 2nd alive
-			unit_1.health = unit_1.health - unit_2.attack  #unit_2 attacks
-			unit_1.is_alive = unit_1.health > 0            #kill when <= 0
+			unit_1.health -= unit_2.attack                 #unit_2 attacks
 		
 	return unit_1.is_alive    #true if 1st wins, otherwise false
